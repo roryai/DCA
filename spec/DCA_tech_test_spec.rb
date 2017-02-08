8d7664c21175110c3685bb49578d92721126983c
Rory Aitchison <rory.aitchison@gmail.com>
2017-02-08 09:07:12 +0000

    third test working

--- spec/DCA_tech_test_spec.rb
+++ spec/DCA_tech_test_spec.rb
@@ -7,10 +7,18 @@ feature 'DCA site test' do
   #   expect(page).to have_content 'Taking care of you'
   # end
 
-  scenario 'can successfully visit "What we do" page' do
-    visit 'https://doctorcareanywhere.com/'
-    click_link('What we do', :match => :first)
-    expect(page.current_url).to eq 'https://doctorcareanywhere.com/what-we-do/' || 'http://doctorcareanywhere.com/what-we-do/'
+  # scenario 'can successfully visit "What we do" page from home page' do
+  #   visit 'https://doctorcareanywhere.com/'
+  #   click_link('What we do', :match => :first)
+  #   expect(page.current_url).to eq 'https://doctorcareanywhere.com/what-we-do/' || 'http://doctorcareanywhere.com/what-we-do/'
+  # end
+
+  scenario 'can successfully visit "FAQs" page from "What we do" page' do
+    visit 'https://doctorcareanywhere.com/what-we-do/'
+    click_link('FAQs', :match => :first)
+    expect(page.current_url).to eq 'https://doctorcareanywhere.com/what-we-do/faqs/'
   end
+
+
 
 end
